1362138f530a36ff9c76153f88a008a3cca4c25b
wtf <wongtf-wa16@student.tarc.edu.my>
2018-10-23 11:20:21 +0800

    Prac2

---
+++ app/src/main/java/com/example/taruc/twoactivities/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.taruc.twoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondAvtivity(View view) {
+        Log.d(LOG_TAG,"Button clicked");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
